46fedeafac9e97d4c017eeceb17460dca6d94482
Allister <kenbradislao@gmail.com>
2018-10-18 09:56:46 +0800

    version0.1

---
+++ androidform/androidform/androidform/Page1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace androidform
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : MasterDetailPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+
+
+        void linker(object sender,System.EventArgs e) {
+
+            Detail = new NavigationPage( new Page2());
+            IsPresented = false;
+
+
+        }
+
+        void linker2(object sender, System.EventArgs e)
+        {
+
+            Detail = new NavigationPage(new Page3());
+            IsPresented = false;
+
+
+        }
+
+    }
+}
